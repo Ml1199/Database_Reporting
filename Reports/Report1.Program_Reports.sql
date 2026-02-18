3b214e6ee4a6ee6b02759cd7a5f027917687d9c7
Mel L <85269434+Ml1199@users.noreply.github.com>
2026-02-18 16:10:12 -0500

    Update Report1.Program_Reports.sql

--- Reports/Report1.Program_Reports.sql
+++ Reports/Report1.Program_Reports.sql
@@ -1,6 +1,30 @@
 -- Objective 1: List the Program_ids, Program Names, along with their respective Start date and End date, sort the output result by Start date, then Program Name.
 -- Objective 2: List the Program_id and Campaign Name related to each Program, sort the output result by Program_id, then Campaign Name.
 
+
+SELECT
+        Program_Id,
+        Program_Name,
+        Start_Date,
+        End_Date
+FROM
+        Program
+ORDER BY 
+        Start_Date, Program_Id
+
+
+
+SELECT 
+        Program_Id,
+        Campaign_Name
+FROM
+        Campaign
+ORDER BY 
+        Program_Id,
+        Campaign_Name
+--
+
+        
 SELECT  p.Program_Name, 
         p.Start_Date, 
         p.End_Date, 
